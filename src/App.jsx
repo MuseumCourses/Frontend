62169a5d32e7045ffe8cea2c7f20be5aaec307d5
GhostServant <nik7nuans@inbox.ru>
2025-01-03 04:11:49 +0300

    Create a project structure and added fonts

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './styles/main.scss';
+function App() {
+  return (
+    <>
+      <div className="container">
+        <h1 className="title_1">
+          Программа повышения квалификации “Просветительская коммуникация в
+          музейном и экскурсионном деле”
+        </h1>
+      </div>
+    </>
+  );
+}
+
+export default App;
