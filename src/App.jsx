76482cd6c2b7707c1d579c5988cc3dac7d0e2346
GhostServant <nik7nuans@inbox.ru>
2025-01-05 05:11:32 +0300

    added component Slider and Relevance, started adapting

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,7 @@
 import InfoCourse from './components/InfoCourse/InfoCourse';
+import Slider from './components/Slider/Slider';
+import Relevance from './components/Relevance/Relevance';
+
 import './styles/main.scss';
 
 function App() {
@@ -6,6 +9,8 @@ function App() {
     <>
       <div className="container">
         <InfoCourse />
+        <Slider />
+        <Relevance />
       </div>
     </>
   );
