7c69a59763f2f6e95f9154f3a64cea6d2e1435ed
GhostServant <nik7nuans@inbox.ru>
2025-01-08 05:39:59 +0300

    Added Cost and Teacher Component with adapting

--- src/components/Cost/Cost.jsx
+++ src/components/Cost/Cost.jsx
@@ -1,7 +1,16 @@
 import styles from './Cost.module.scss';
 
 const Cost = () => {
-  return <div className={styles.container}>Content</div>;
+  return (
+    <section className={styles.cost_section}>
+      <div className={styles.cost_section_circle}>
+        <span className={styles.cost_section_text}>СТОИМОСТЬ КУРСА</span>
+      </div>
+      <div className={styles.cost_section_apply_button}>
+        <a href="#">ОСТАВИТЬ ЗАЯВКУ</a>
+      </div>
+    </section>
+  );
 };
 
 export default Cost;
